699ea66b3b0d9c94ed97483290649ec076b83c5e
NPhucs <phucnguyen11204@gmail.com>
2025-07-21 00:04:04 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = 3000;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test")
+  .then(() => console.log("Connected!"))
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server dang chay o cong ${PORT}`);
+    });
+  });
