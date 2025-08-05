59ba9a52b8f73b8d33e35f8d24d2629bf89770c2
NPhucs <phucnguyen11204@gmail.com>
2025-08-05 20:19:24 +0700

    api color

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import authRouter from "./auth.routes.js";
 import categoryRouter from "./category.routes.js";
 import brandRouter from "./brand.routes.js";
 import sizeRouter from "./size.routes.js";
+import colorRouter from "./color.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/categories", categoryRouter);
 router.use("/brands", brandRouter);
-router.use("/sizes", sizeRouter)
+router.use("/sizes", sizeRouter);
+router.use("/colors", colorRouter)
 
 export default router;
